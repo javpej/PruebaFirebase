f6f5679c0208e2c272f5f62aec0b32a8f9f9e6d7
Javi Perán <javperan@gmail.com>
2017-06-01 14:43:04 +0200

    Termina interfaz tarjetas

--- app/src/main/java/com/example/javi/pruebafirebase/MainActivity.java
+++ app/src/main/java/com/example/javi/pruebafirebase/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.javi.pruebafirebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView myrecyclerview;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.MyToolbar);
+        setSupportActionBar(toolbar);
+        myrecyclerview = (RecyclerView) findViewById(R.id.MyRecyclerView);
+
     }
 }
